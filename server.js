36e9ab793088447992e38b8f88e3e43b6e4b22c2
carlos676767 <carlitobr730@gmail.com>
2024-06-01 14:47:47 -0700

    api rest

---
+++ server.js
@@ -0,0 +1,36 @@
+
+const express = require("express")
+const { MongoClient } = require("mongodb");
+require('dotenv').config()
+
+const expressApi = express()
+
+//get router
+expressApi.get("/dados", (req, response) => {
+    try {
+        response.send({ status: "OK", msg: "the connection was successful" });
+    } catch (error) {
+        response.status(500).send({ error: "Internal Server Error", status: 500 });
+    }
+});
+
+const url = `mongodb+srv://admin:${process.env.DATABASE_PASS}@dados.7d94myt.mongodb.net/?retryWrites=true&w=majority&appName=dados`
+const client = new MongoClient(url);
+
+const dataBaseConnect = async () => {
+    try {
+        await client.connect()
+        const dataBaseName = client.db('database')
+        const collection = dataBaseName.collection("database")
+        const dataBaseItensAdd = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+        console.log(dataBaseItensAdd);
+    } catch (error) {
+        console.error("error connect database")
+    }
+}
+
+dataBaseConnect()
+const port = 8070
+expressApi.listen(port, () => {
+    console.log(`Server running on the port ${port}...`);
+})
