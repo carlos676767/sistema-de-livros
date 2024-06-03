001765f827a31aba724c1e7b8e52e81d771aae89
carlos676767 <carlitobr730@gmail.com>
2024-06-03 11:57:46 -0700

    ok

--- server.js
+++ server.js
@@ -4,49 +4,52 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const expressApi = express();
 expressApi.use(bodyParser.json());
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI("AIzaSyCL432ibOPgBawbZW_4P4K4yWCR6wKxufI");
+
 
 expressApi.post("/cadastro", (req, resposta) => {
-  try {
-    resposta.send({ status: "OK", msg: "livro cadastrado" });
-    const reqOk = req.body;
-    console.log(reqOk);
-    dataBaseConnect(reqOk);
-  } catch (error) {
-    resposta.status(500).send({ error: "Internal Server Error", status: 500 });
-  }
+    try {
+        resposta.send({ status: "OK", msg: "livro cadastrado" });
+        const reqOk = req.body;
+        console.log(reqOk);
+        dataBaseConnect(reqOk);
+    } catch (error) {
+        resposta.status(500).send({ error: "Internal Server Error", status: 500 });
+    }
 });
 
 const url = `mongodb+srv://admin:${process.env.DATABASE_PASS}@dados.7d94myt.mongodb.net/?retryWrites=true&w=majority&appName=dados`;
 const client = new MongoClient(url);
 const dataBaseConnect = async (dados) => {
-  try {
-    await client.connect();
-    const dataBaseName = client.db("database");
-    const collection = dataBaseName.collection("database");
-    collection.insertOne(dados);
-  } catch (error) {
-    console.error("error connect database");
-  } finally {
-    setTimeout(() => {
-      client.close();
-    }, 10000);
-  }
+    try {
+        await client.connect();
+        const dataBaseName = client.db("database");
+        const collection = dataBaseName.collection("database");
+        collection.insertOne(dados);
+    } catch (error) {
+        console.error("error connect database");
+    } finally {
+        setTimeout(() => {
+            client.close();
+        }, 10000);
+    }
 };
 
 expressApi.get("/dados:dadoshttp", (req, response) => {
-  try {
-    const dadosHttpGet = req.params.dadoshttp.slice(1, Infinity);
-    const dados = buscarLivro(dadosHttpGet);
-    dados.then((data) => {
-      response.send({
-        status: "OK",
-        msg: "dados consultados com sucesso.",
-        dados: data,
-      });
-    });
-  } catch (error) {
-    response.status(500).send({ error: "Internal Server Error", status: 500 });
-  }
+    try {
+        const dadosHttpGet = req.params.dadoshttp.slice(1, Infinity);
+        const dados = buscarLivro(dadosHttpGet);
+        dados.then((data) => {
+            response.send({
+                status: "OK",
+                msg: "dados consultados com sucesso.",
+                dados: data,
+            });
+        });
+    } catch (error) {
+        response.status(500).send({ error: "Internal Server Error", status: 500 });
+    }
 });
 
 expressApi.delete("/deletar:delete", (req, response) => {
@@ -61,35 +64,48 @@ expressApi.delete("/deletar:delete", (req, response) => {
 });
 
 async function deletarLivro(livro) {
-  try {
-    await client.connect();
-    const dataBaseName = client.db("database");
-    const collection = dataBaseName.collection("database");
-    const searchName = await collection.deleteOne({ tituloLivro: livro });
-    console.log(searchName);
-  } catch (error) {
-    console.error("error connect database");
-  } finally {
-    client.close();
-  }
+    try {
+        await client.connect();
+        const dataBaseName = client.db("database");
+        const collection = dataBaseName.collection("database");
+        const searchName = await collection.deleteOne({ tituloLivro: livro });
+        console.log(searchName);
+    } catch (error) {
+        console.error("eror em deletar livro.");
+    } finally {
+        client.close();
+    }
 }
 
 async function buscarLivro(dados) {
-  try {
-    await client.connect();
-    const dataBaseName = client.db("database");
-    const collection = dataBaseName.collection("database");
-    const searchName = await collection.findOne({ tituloLivro: dados });
-    console.log(searchName);
-    return searchName;
-  } catch (error) {
-    console.error("error connect database");
-  } finally {
-    client.close();
-  }
+    try {
+        await client.connect();
+        const dataBaseName = client.db("database");
+        const collection = dataBaseName.collection("database");
+        const searchName = await collection.findOne({ tituloLivro: dados });
+        console.log(searchName);
+        return searchName;
+    } catch (error) {
+        console.error("error em buscar livro.");
+    } finally {
+        client.close();
+    }
 }
+
+async function recomendarLivros(gosto) {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const prompt = `Oi Estou à procura de novas leituras e gostaria de recomendações 
+    personalizadas. 
+    Eu gosto muito de gêneros como, ${gosto}, apenas me mande os livros sem me perguntar mais nada`;
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+recomendarLivros();
 
 const port = 8080;
 expressApi.listen(port, () => {
-  console.log(`Server running on the port ${port}...`);
+    console.log(`Server running on the port ${port}...`);
 });
